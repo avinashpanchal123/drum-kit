6593e7d41a37e2efd9737daa9902db939ff1e4dd
Avinash Panchal <88651615+avinashpanchal123@users.noreply.github.com>
2022-03-02 21:26:20 +0530

    added sound files to btn-click event
    also added keyboard event listeners to check for key presses

--- index.js
+++ index.js
@@ -1,12 +1,65 @@
-let set = document.getElementsByClassName("drum")
+let set = document.querySelectorAll(".drum");
 
-// set.forEach(ele => {
-//    ele.addEventListener("click", myFunction);
-// });
+let audios = [
+  "sounds/tom-1.mp3",
+  "sounds/tom-2.mp3",
+  "sounds/tom-3.mp3",
+  "sounds/tom-4.mp3",
+  "sounds/snare.mp3",
+  "sounds/crash.mp3",
+  "sounds/kick-bass.mp3",
+];
+
+for (let i = 0; i < set.length; i++) {
+  set[i].addEventListener("click", () => {
+    var audio = new Audio(audios[i]);
+    audio.play();
+  });
+}
+
+document.addEventListener("keydown", (e) => {
+    let keyPressed = e.key;
+    console.log(e);
+    makeSound(keyPressed)
+});
 
 
-console.log((set));
+function makeSound(key){
+    
 
-function myFunction() {
-  alert("fgdgdfs")
+    switch (key) {
+      case "w":
+        var audio = new Audio(audios[0]);
+        audio.play();
+        break;
+      case "a":
+        var audio = new Audio(audios[1]);
+        audio.play();
+        break;
+      case "s":
+        var audio = new Audio(audios[2]);
+        audio.play();
+        break;
+      case "d":
+        var audio = new Audio(audios[3]);
+        audio.play();
+        break;
+      case "j":
+        var audio = new Audio(audios[4]);
+        audio.play();
+        break;
+      case "k":
+        var audio = new Audio(audios[5]);
+        audio.play();
+        break;
+      case "i":
+        var audio = new Audio(audios[6]);
+        audio.play();
+        break;
+     
+  
+      default:
+          console.log(keyPressed);
+        break;
+    }
 }
