ff274343a13af6db48f301310175e6c41b601838
Avinash Panchal <88651615+avinashpanchal123@users.noreply.github.com>
2022-03-02 17:45:06 +0530

    Added sounds and images files

---
+++ index.js
@@ -0,0 +1,12 @@
+let set = document.getElementsByClassName("drum")
+
+// set.forEach(ele => {
+//    ele.addEventListener("click", myFunction);
+// });
+
+
+console.log((set));
+
+function myFunction() {
+  alert("fgdgdfs")
+}
